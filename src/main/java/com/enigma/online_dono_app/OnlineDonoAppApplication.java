c6f3a8d8f425b187120cb9087af7f37faaeffa0e
Farrel Akbar <farrelakbar432@gmail.com>
2024-12-08 10:06:48 +0700

    initial commit

---
+++ src/main/java/com/enigma/online_dono_app/OnlineDonoAppApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.online_dono_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDonoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDonoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/online_dono_app/OnlineDonoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.online_dono_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDonoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
